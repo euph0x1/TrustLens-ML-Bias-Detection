0df39340b9ff11f49f85e1b688aa31d908a89c4b
euph0x1 <ahmedfaria99@gmail.com>
2026-06-25 11:15:41 +0530

    commit

--- src/trustlens/fairness/__init__.py
+++ src/trustlens/fairness/__init__.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
-from trustlens.fairness.counterfactual import CounterfactualEvaluator, CounterfactualScenario
+from trustlens.fairness.counterfactual import CounterfactualEvaluator
 from trustlens.fairness.metrics import (
-    disparate_impact_ratio,
-    fairness_disparity,
-    positive_framing_score,
     statistical_parity_difference,
+    disparate_impact_ratio,
+    equal_opportunity_difference,
+    equalized_odds_difference,
+    overall_fairness_score,
+    get_certification_status
 )
 
 __all__ = [
     "CounterfactualEvaluator",
-    "CounterfactualScenario",
-    "disparate_impact_ratio",
-    "fairness_disparity",
-    "positive_framing_score",
     "statistical_parity_difference",
+    "disparate_impact_ratio",
+    "equal_opportunity_difference",
+    "equalized_odds_difference",
+    "overall_fairness_score",
+    "get_certification_status"
 ]
